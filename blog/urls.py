95dbc281d5ee572ba336c0b2c37b25c073e8009d
Christina  Matson <cmatson93@gmail.com>
2018-02-14 22:44:35 -0800

    trying to add slug

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # path('help', views.post_help, name='help'),
+    path('<slug:slug>/', views.post_detail , name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ def post_list(request):
     posts = Post.objects.all().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request):
+    post = Post.objects.get(slug= slug)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
