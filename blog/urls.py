102266ea50439931849845ba570baa001763486d
Christina  Matson <cmatson93@gmail.com>
2018-02-27 10:53:42 -0800

    article detail page styling

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ app_name = "blog"
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('/post/<slug:slug>/', views.post_detail , name='detail'),
+    path('post/<slug:slug>/', views.post_detail , name='detail'),
     path('about/', views.about_page, name='about_page'),
 ]
